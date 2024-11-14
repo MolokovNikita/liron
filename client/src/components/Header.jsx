b478f41062bdbbecef37456cb650c18a634b27e5
DekuFoo <ggmaster039@gmail.com>
2024-11-14 12:38:54 +0300

    Working on main page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,11 +2,35 @@ import styles from "../styles/header.module.css";
 export default function Header(props) {
   return (
     <header>
-   
+     <div className={styles.top__header}>
+        <div className={styles.top_left__container}>
+          <p className={styles.city__label}>
+            <a href="#">
+              г. Москва
+            </a>{" "}
+          </p>
+        </div>
+        <div className={styles.top_right__container}>
+          <ul className={styles.top_right__list}>
+            <li>
+              <a href="#">Ежедневно с 9:00 до 21:00</a>
+            </li>
+            
+            <li>
+              <a href="#">+7 495 362-72-86</a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div className={styles.bottom__header}>
-        <a href="/">
-          <img className={styles.logo} src="/logo.jpeg" />
+        <a href="/" className={styles.logo__href}>
+          <img className={styles.logo} src="/main_logo.png" />
+          <div className={styles.logo__label}>
+        <i>Матрасы для фур</i>
+        </div>
         </a>
+     
+        
         <div className={styles.left__container}>
           <ul className={styles.left_nav__list}>
             <li className={styles.catalor__item}>
@@ -24,6 +48,17 @@ export default function Header(props) {
           </ul>
         </div>
         <div className={styles.right__container}>
+        <ul className={styles.right_nav__list}>
+              <li className={styles.basket__item}>
+                <img className ={styles.basket__icon}src="/basket.png" alt="" />
+              <a to="/basket">
+                Корзина
+                <div className={styles.basket__counter}>
+                  0
+                </div>
+              </a>
+            </li>
+        </ul>
         </div>
       </div>
     </header>
--- client/src/pages/main-page.jsx
+++ client/src/pages/main-page.jsx
@@ -1,10 +1,41 @@
 import Header from '../components/Header.jsx'
 import Footer from '../components/Footer.jsx'
-export default function MainPage(){
+import styles from '../styles/main-content.module.css'
+export default function MainPage() {
     return (
         <>
-        <Header/>
-        <Footer/>
+            <Header />
+            <div className={styles.topic__container}>
+            <h1>
+            Комфорт в дороге – ваш идеальный матрас для грузовика!
+            </h1>
+            <div className={styles.topic_text__container}>
+                <div className={styles.topic_text__left}>
+                <p className={styles.topic_container__text}>
+            Изготавливаем матрасы для всех марок и моделей грузовиков с применением уникальной 
+            технологии. 
+            </p>
+            <p className={styles.topic_container__text}>
+            Продукция создается точно по размерам штатных спальных мест, 
+            чтобы обеспечить максимальное удобство в пути для водителей DAF, VOLVO, MAN, MERCEDES-BENZ,
+             SCANIA, IVECO, RENAULT, FORD, и многих других.
+            </p>
+                </div>
+            <div className={styles.topic_text__right}>
+                <img src="/daf_big.jpg" alt="" className={styles.company__logo} />
+                <img src="/volvo_big.jpg" alt="" className={styles.company__logo} />
+                <img src="/man_big.jpg" alt="" className={styles.company__logo} />
+                <img src="/mers_big.jpg" alt="" className={styles.company__logo} />
+                <img src="/scania_big.png" alt="" className={styles.company__logo} />
+                <img src="/iveco_big.jpg" alt="" className={styles.company__logo} />
+                <img src="/renault_big.jpg" alt="" className={styles.company__logo} />
+                <img src="/ford_big.jpg" alt="" className={styles.company__logo} />
+            </div>
+            </div>
+            
+            </div>
+            
+            <Footer />
         </>
     )
 }
